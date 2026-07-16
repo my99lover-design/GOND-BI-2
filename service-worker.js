af828ca2cc3bd381b673c81463936f5c387fb1fa
my99lover-design <my99lover@gmail.com>
2026-07-16 10:04:13 +0900

    26.07.16 버튼수정

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v34";
+const CACHE_VERSION = "v35";
 const APP_CACHE = `gimpo-b-app-${CACHE_VERSION}`;
 const IMAGE_CACHE = `gimpo-b-images-v4`;
 const DATA_CACHE = `gimpo-b-data-v5`;
@@ -8,8 +8,8 @@ const NAVIGATION_TIMEOUT_MS = 2000;
 const APP_SHELL = [
     "./",
     "./index.html",
-    "./style.css?v=20260716-9",
-    "./script.js?v=20260716-9",
+    "./style.css?v=20260716-10",
+    "./script.js?v=20260716-10",
     "./manifest.json",
     "./icons/icon-180.png",
     "./icons/icon-192.png",
