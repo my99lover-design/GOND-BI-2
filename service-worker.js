e4a839e21b82993eb82cee4348762b20480cece2
my99lover-design <my99lover@gmail.com>
2026-07-21 17:57:39 +0900

    260721 프모 글자크기수정

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v57";
+const CACHE_VERSION = "v58";
 const APP_CACHE = `gimpo-b-app-${CACHE_VERSION}`;
 const IMAGE_CACHE = `gimpo-b-images-v4`;
 const DATA_CACHE = `gimpo-b-data-v5`;
@@ -8,10 +8,10 @@ const NAVIGATION_TIMEOUT_MS = 2000;
 const APP_SHELL = [
     "./",
     "./index.html",
-    "./style.css?v=20260716-32",
-    "./number-one.css?v=20260716-32",
-    "./script.js?v=20260716-32",
-    "./number-one.js?v=20260716-32",
+    "./style.css?v=20260716-33",
+    "./number-one.css?v=20260716-33",
+    "./script.js?v=20260716-33",
+    "./number-one.js?v=20260716-33",
     "./manifest.json",
     "./icons/icon-180.png",
     "./icons/icon-192.png",
