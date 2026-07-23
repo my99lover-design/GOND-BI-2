3c4d2503623c7ccced74ac734b3a2d49d9bf67b7
my99lover-design <my99lover@gmail.com>
2026-07-23 09:55:58 +0900

    260723 tnfkrrjwjf ansrn tnwjd

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-/* 넘버원 김포B 공비 - 거절가능 80% 기준 분리 20260716-43 */
+/* 넘버원 김포B 공비 - B마트/스토어 제외 안내 20260716-44 */
 const APP_BOOT_STARTED_AT = performance.now();
 const API_URL = "https://script.google.com/macros/s/AKfycbyFbQUILKYrMZEfGl8tXPHThYEK1ncyU0JV36Dbfiqi5cdFRKY06PQUS4IwHDDLW8boIA/exec";
 const LOCATIONS_URL = "./locations.json";
@@ -4617,14 +4617,14 @@ async function recoverFromSafeMode() {
 }
 
 const DIAGNOSTIC_CACHE_NAMES = Object.freeze({
-    app: "gimpo-b-app-v68",
+    app: "gimpo-b-app-v69",
     images: "gimpo-b-images-v4",
     data: "gimpo-b-data-v5",
     runtime: "gimpo-b-runtime-v3"
 });
 
 const DIAGNOSTIC_APP_SHELL = Object.freeze([
-    "./", "./index.html", "./style.css?v=20260716-43", "./number-one.css?v=20260716-36", "./script.js?v=20260716-43", "./number-one.js?v=20260716-36", "./manifest.json",
+    "./", "./index.html", "./style.css?v=20260716-44", "./number-one.css?v=20260716-36", "./script.js?v=20260716-44", "./number-one.js?v=20260716-36", "./manifest.json",
     "./icons/icon-180.png", "./icons/icon-192.png", "./icons/icon-512.png"
 ]);
 const DIAGNOSTIC_GATE_IMAGES = Object.freeze([
@@ -4807,7 +4807,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 /* ========================= 성능 판정 현실화 v24 ========================= */
-const FINAL_BUILD_INFO = Object.freeze({ fileVersion: "20260716-43", serviceWorkerVersion: "v68" });
+const FINAL_BUILD_INFO = Object.freeze({ fileVersion: "20260716-44", serviceWorkerVersion: "v69" });
 const SAFE_MODE_BUILD_KEY = "gimpoB_safe_mode_build_v1";
 (function clearStaleSafeModeAfterBuildUpdate() {
     try {
@@ -5188,8 +5188,8 @@ collectDiagnostics = async function collectDiagnosticsV23() {
 
 /* ========================= v25 전체 UI 정합성 최적화 ========================= */
 const V25_UI_CONFIG = Object.freeze({
-    fileVersion: "20260716-43",
-    serviceWorkerVersion: "v68",
+    fileVersion: "20260716-44",
+    serviceWorkerVersion: "v69",
     statusTimestampMaxAge: 10 * 60 * 1000,
     minimumBusyMs: 450
 });
